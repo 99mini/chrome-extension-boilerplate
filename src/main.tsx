6b629313e6b16fef7650b9e838018b89e9a4e1cb
99mini <itman390@gmail.com>
2024-10-22 04:18:16 +0900

    feat: mock api

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,11 @@
 import React, { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import App from './App';
+import mock from './mock';
+
 import './index.css';
+
+mock();
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,17 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-export default defineConfig({
+export default defineConfig(({ mode }) => ({
   plugins: [react()],
   build: {
     outDir: 'dist',
     rollupOptions: {
       input: {
-        newtab: 'index.html'
-      }
-    }
-  }
-});
+        newtab: 'index.html',
+      },
+    },
+  },
+  define: {
+    MOCK_CHROME: mode === 'development',
+  },
+}));
